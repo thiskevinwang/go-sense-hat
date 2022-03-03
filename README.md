c703c770d155d5308c9180fa87d12514f93da9cd
Kevin Wang <kwangsan@gmail.com>
2022-03-03 15:31:07 +0000

    draw heart

---
+++ README.md
@@ -0,0 +1,1 @@
+# Go Sense Hat
--- main.go
+++ main.go
@@ -8,23 +8,44 @@ import (
 
 	"github.com/lanzafame/bobblehat/sense/screen"
 	"github.com/lanzafame/bobblehat/sense/screen/color"
-	"github.com/lanzafame/bobblehat/sense/screen/texture"
 	"github.com/lanzafame/bobblehat/sense/stick"
 )
 
 func main() {
 	fmt.Println("Hello, world")
 
-	fb := screen.NewFrameBuffer()
-	fb.SetPixel(0, 5, color.Red)
-	fb.SetPixel(1, 2, color.Green)
-	fb.SetPixel(3, 4, color.White)
-	fb.SetPixel(5, 5, color.Blue)
-	fb.SetPixel(7, 7, color.Magenta)
-	screen.Draw(fb)
+	//red
+	r := color.New(255, 0, 0)
+	//maroon
+	m := color.New(100, 0, 0)
+	//pink
+	p := color.New(255, 192, 203)
+	//gray
+	g := color.New(80, 80, 80)
+	//off
+	o := color.Black
 
-	tx := texture.New(16, 16)
-	tx.SetPixel(8, 8, color.White)
+	fb := screen.NewFrameBuffer()
+
+	// the grid indices run Top-Right to Bottom-Left
+	grid := [8][8]color.Pixel565{
+		{o, o, o, o, o, o, o, o},
+		{o, g, g, o, o, g, g, o},
+		{g, m, m, g, g, p, p, g},
+		{g, m, m, r, r, r, p, g},
+		{g, m, m, m, r, r, r, g},
+		{o, g, m, m, m, r, g, o},
+		{o, o, g, m, m, g, o, o},
+		{o, o, o, g, g, o, o, o},
+	}
+
+	for i, row := range grid {
+		for j, color := range row {
+			fb.SetPixel(i, j, color)
+		}
+	}
+
+	screen.Draw(fb)
 
 	// Capture Ctrl+C
 	c := make(chan os.Signal, 1)
